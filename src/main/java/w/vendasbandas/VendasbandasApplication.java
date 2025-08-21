d7501d358a1a0235c32d74a72864f326760db418
Kevin040106 <kevin.carvalho@dcomp.ufs.br>
2025-08-21 16:40:26 -0300

    Criação de VendasbandasApplication.java
    Para inicializar a aplicação do Spring Boot

---
+++ src/main/java/w/vendasbandas/VendasbandasApplication.java
@@ -0,0 +1,11 @@
+package w.vendasbandas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //inicialização do springboot
+public class VendasbandasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VendasbandasApplication.class, args);
+    }
+}
