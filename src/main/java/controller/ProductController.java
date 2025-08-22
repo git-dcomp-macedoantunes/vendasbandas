8b896d087596de80165c91a1fe6e4789e86b4fdb
Diogo Lima <diogo.lima@dcomp.ufs.br>
2025-08-21 22:36:23 -0300

    Adição ao ProductController e criação do UserController

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -2,11 +2,13 @@ package controller;
 import java.util.ArrayList;
 import mediator.LogMediator;
 import model.ProductModel;
+import model.UserModel;
 import model.UserSellerModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,46 +26,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProductController {
     
         private final LogMediator service;
-        private UserSellerModel vendedor;
+        private ArrayList <ProductModel> allProducts;
 
         
-        public ProductController (LogMediator action, UserSellerModel vendedor) {
+        public ProductController (LogMediator action, UserModel user) {
             this.service = action;
-            this.vendedor = vendedor;
-            
         }
-        //Pega todos os produtos do servidor;
-           @GetMapping
-           public ArrayList<ProductModel> getProducts(){
-               return service.getProductList (vendedor); 
+        
+        //Pag principal de produtos todos os produtos do site
+         @GetMapping ("/product")
+           public ArrayList<ProductModel> getAllProducts(ArrayList<UserSellerModel> sellers){
+               for (int i=0; i < sellers.size(); i++){
+               allProducts.addAll(sellers.get(i).getProductList());
+               }
+              return  allProducts;
            }
+        
+        //Olha o carrinho para cliente
+        //Olha a lista de produtos a venda para vendedor
+           @GetMapping ("/carrinho/{user}")
+           public ArrayList<ProductModel> getProducts(@PathVariable UserModel user){
+               return service.getProductList (user);
+           }
+           
            
          //Pega um produto especifico do servidor;
-           @GetMapping 
-           public ProductModel getProductByName(String productName){
-               return service.findProductByName(productName); 
+           @GetMapping ("/product/{name}")
+           public ProductModel getProductByName(@PathVariable String name){
+               return service.findProductByName(name); 
            }
            
-           //Deleta um produto especifico do servidor.
-           @PutMapping
-           public boolean deleteProduct (String productName){
-               return service.getProductList(vendedor).remove(service.findProductByName(productName));
+           //Deleta um produto especifico do servidor se for vendedor
+           //Tira do carrinho se for cliente
+           @DeleteMapping ("/product/{user}/delete_{name}_da_lista")
+           public String deleteProduct (@PathVariable String name, @PathVariable UserModel user){
+                service.getProductList(user).remove(service.findProductByName(name));
+               return "redirect:/product/{user}";
            }
            
-           //cria um produto novo no servidor
-           @PostMapping
-           public void createProduct(@RequestBody ProductModel product) {
-               service.logProduct(product.getName(), product.getPrice(), vendedor, product.getDescription(), product.getStock());
+           //cria um produto novo no servidor para vendedores apenas
+           @PostMapping ("/{product}")
+           public String createProduct(@RequestBody ProductModel product, @PathVariable UserModel user) {
+               if (user.getClass() == UserSellerModel.class){
+               service.logProduct(product.getName(), product.getPrice(), user, product.getDescription(), product.getStock());
+                }
+            return "redirect:/product";
            }
-
-           //Ve qual seller foi escolhido
-           public UserSellerModel getSeller (){
-               return this.vendedor;
-           }
-           
-           //Coloca o seller escolhido
-           @PutMapping
-           public void setSeller (UserSellerModel vendedor){
-               this.vendedor = vendedor;
-           }
-           }
+}
